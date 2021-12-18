6b5f85cdde6ec9fd1475eac564df9f53c0701551
White_Devil <pashanehal8@gmail.com>
2021-12-18 15:04:16 +0500

    Initial commit

---
+++ app/src/main/java/com/example/assignment5/third_page.java
@@ -0,0 +1,14 @@
+package com.example.assignment5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class third_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_page);
+    }
+}
